4380ffb741511c8a6c67fe1a47fc75d3b3040567
Mukesh <mukesh.chugwani@myglamm.com>
2023-02-19 22:35:20 +0530

    attendance class and recurrence implementation

--- attendance.py
+++ attendance.py
@@ -1,13 +1,28 @@
+from functools import lru_cache
+
+
 class Attendance:
     def __init__(self, total_classes, miss_allowed):
+        """
+        :param total_classes: the total no of classes that are going to be there
+        :param miss_allowed: maximum (consecutive days + 1) allowed to miss
+        """
+        if total_classes < miss_allowed or total_classes < 0 or miss_allowed < 0:
+            raise Exception("Invalid Inputs")
         self.total_classes = total_classes
         self.miss_allowed = miss_allowed
 
     def calculate_ways_to_attend_classes(self):
-        pass
+        return self._calculate_attendance(self.total_classes, 0)
 
-    def calculate_probability_for_grad_miss(self):
-        pass
+    def calculate_ways_to_miss_grad_day(self):
+        return self._calculate_attendance(self.total_classes - 1, 1)
 
-    def _calculate_attendance(self, total_classes, miss):
-        pass
+    @lru_cache(maxsize=None)
+    def _calculate_attendance(self, total_classes, missed):
+        if self.miss_allowed == missed:
+            return 0
+        if total_classes == 0:
+            return 1
+        return self._calculate_attendance(total_classes - 1, 0) + self._calculate_attendance(total_classes - 1,
+                                                                                             missed + 1)
--- main.py
+++ main.py
@@ -8,8 +8,11 @@ if __name__ == "__main__":
         total_classes = int(sys.argv[1])
         print("Total Number of Classes are {}".format(total_classes))
     except IndexError:
-        print("Please pass 'total classes' for the program to run")
+        print("'total classes' argument is required")
     except Exception as e:
         print(e)
     else:
         attendance = Attendance(total_classes, max_miss_allowed)
+        answer_1 = attendance.calculate_ways_to_attend_classes()
+        answer_2 = attendance.calculate_ways_to_miss_grad_day()
+        print(f'{answer_2}/{answer_1}')
